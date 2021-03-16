e60590ffb7102204f229681eb4271b43136f6675
Sagar Maharjan <sagar.maharjan@palmmind.com>
2021-03-16 21:35:11 +0545

    understanding event binding

--- app.js
+++ app.js
@@ -1,30 +1,10 @@
-// Vue -> global object
 const app = Vue.createApp({
-    // configuration of the app; specific property names
-    // takes all data that u return and merge into global vue instance object.
-    // your methods are also available there.
 
     data() {
-        // always return object
         return {
-            // prop name is upto you; we can pass any data types->array, object, whatever you like
-            courseGoalA: 'Finish the course and learn vue.',
-            courseGoalB: '<h2>Master and build amazing vue apps.</h2>',
-            vueLink: 'https://vuejs.org'
-        }
+            counter: 0
+        };
     },
-    // methods will be object full of function
-    methods: {
-        outputGoal() {
-            const randomNumber = Math.random();
-            if (randomNumber < 0.5) {
-                return this.courseGoalA;
-            } else {
-                return this.courseGoalB;
-            }
-        }
-    }
 });
 
-// dom which we wanna control
-app.mount('#user-goal');
+app.mount('#events');
