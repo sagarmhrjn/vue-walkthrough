e99eee866f031a0d9759dffde4d726334f3c25e0
Sagar Maharjan <sagar.maharjan@palmmind.com>
2021-03-17 01:02:25 +0545

    hint for watcher and computed prop

--- app.js
+++ app.js
@@ -4,17 +4,47 @@ const app = Vue.createApp({
         return {
             counter: 10,
             name: '',
+            lastName: '',
+            // fullname: '',
             confirmedName: ''
         };
     },
+    // repeat another data or computed prop name in a watcher method as a method name.
+    // that watcher method will be automatically be executed by vue.
+    // if you wanna run some code which some maybe but not necessarily update some data propery
+    watch: {
+        counter(value) {
+            if (value > 50) {
+                const that = this;
+                setTimeout(function () {
+                    that.counter = 0;
+                }, 2000)
+            }
+        }
+        // name(value) {
+        //     if (value === '') {
+        //         this.fullname = '';
+        //     } else {
+        //         this.fullname = value + ' ' + this.lastName;
+        //     }
+        // },
+        // lastName(value) {
+        //     if (value === '') {
+        //         this.fullname = '';
+        //     } else {
+        //         this.fullname = this.name + ' ' + value;
+        //     }
+        // }
+    },
     // we use computed property like variable not a function
+    // if you wanna calculate some value dynamically
     computed: {
         fullname() {
             console.log('Running again...')
-            if (this.name === '') {
+            if (this.name === '' || this.lastName === '') {
                 return ''
             }
-            return this.name + ' ' + 'Maharjan';
+            return this.name + ' ' + this.lastName;
         }
     },
     methods: {
