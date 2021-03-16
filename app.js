189d04dd780a9005cd2ccb734923df48dff06db6
Sagar Maharjan <sagar.maharjan@palmmind.com>
2021-03-16 22:21:49 +0545

    using the natie event object;input

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const app = Vue.createApp({
 
     data() {
         return {
-            counter: 0
+            counter: 0,
+            name: ''
         };
     },
     methods: {
+        setName(event, lastname) {
+            this.name = event.target.value + ' ' + lastname;
+        },
         add(num) {
             this.counter = this.counter + 5;
         },
