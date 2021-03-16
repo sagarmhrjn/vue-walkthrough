17f18115975b99551e2ba67ae2f4683a11876f1f
Sagar Maharjan <sagar.maharjan@palmmind.com>
2021-03-17 01:20:00 +0545

    dynamic styling with inline styles

--- app.js
+++ app.js
@@ -2,74 +2,23 @@ const app = Vue.createApp({
 
     data() {
         return {
-            counter: 10,
-            name: '',
-            lastName: '',
-            // fullname: '',
-            confirmedName: ''
-        };
-    },
-    // repeat another data or computed prop name in a watcher method as a method name.
-    // that watcher method will be automatically be executed by vue.
-    // if you wanna run some code which some maybe but not necessarily update some data propery
-    watch: {
-        counter(value) {
-            if (value > 50) {
-                const that = this;
-                setTimeout(function () {
-                    that.counter = 0;
-                }, 2000)
-            }
-        }
-        // name(value) {
-        //     if (value === '') {
-        //         this.fullname = '';
-        //     } else {
-        //         this.fullname = value + ' ' + this.lastName;
-        //     }
-        // },
-        // lastName(value) {
-        //     if (value === '') {
-        //         this.fullname = '';
-        //     } else {
-        //         this.fullname = this.name + ' ' + value;
-        //     }
-        // }
-    },
-    // we use computed property like variable not a function
-    // if you wanna calculate some value dynamically
-    computed: {
-        fullname() {
-            console.log('Running again...')
-            if (this.name === '' || this.lastName === '') {
-                return ''
-            }
-            return this.name + ' ' + this.lastName;
+            boxASelected: false,
+            boxASelected: false,
+            boxASelected: false,
         }
     },
     methods: {
-        outputFullName() {
-
-        },
-        confirmInput() {
-            this.confirmedName = this.name
-        },
-        submitForm(event) {
-            alert('submitted')
-        },
-        setName(event) {
-            this.name = event.target.value;
-        },
-        add(num) {
-            this.counter = this.counter + 5;
-        },
-        reduce(num) {
-            this.counter = this.counter - 5;
-        },
-        resetInput() {
-            this.name = '';
+        boxSelected(box) {
+            if (box === 'A') {
+                this.boxASelected = true;
+            } else if (box === 'B') {
+                this.boxASelected = true;
+            } else if (box === 'C') {
+                this.boxASelected = true;
+            }
         }
     }
+
 });
 
-app.mount('#events');
+app.mount('#styling');
