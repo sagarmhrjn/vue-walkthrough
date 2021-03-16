25a565d55c7cf132bcd296f48b5d22bb65ef4805
Sagar Maharjan <sagar.maharjan@palmmind.com>
2021-03-16 21:40:01 +0545

    events and methods

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const app = Vue.createApp({
             counter: 0
         };
     },
+    methods: {
+        add() {
+            this.counter++
+        },
+        reduce() {
+            this.counter--
+        }
+    }
 });
 
 app.mount('#events');
