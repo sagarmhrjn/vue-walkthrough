a83e3e9066a055e4b18a834df1a6bd25f3aa73ec
Sagar Maharjan <sagar.maharjan@palmmind.com>
2021-03-17 22:45:50 +0545

    rendering content conditionally

--- app.js
+++ app.js
@@ -2,28 +2,16 @@ const app = Vue.createApp({
 
     data() {
         return {
-            boxASelected: false,
-            boxBSelected: false,
-            boxCSelected: false,
-        }
-    },
-    computed: {
-        boxAClasses(){
-            return {active:this.boxASelected}
+            enteredGoalValue: '',
+            goals: []
         }
     },
     methods: {
-        boxSelected(box) {
-            if (box === 'A') {
-                this.boxASelected = !this.boxASelected;
-            } else if (box === 'B') {
-                this.boxBSelected = !this.boxBSelected;
-            } else if (box === 'C') {
-                this.boxCSelected = !this.boxCSelected;
-            }
+        addGoal() {
+            this.goals.push(this.enteredGoalValue)
         }
     }
 
 });
 
-app.mount('#styling');
+app.mount('#user-goals');
