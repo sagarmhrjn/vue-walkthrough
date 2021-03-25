a5f9c003b473c7514fac8265dcd3cd6969cf6cd9
Sagar Maharjan <sagar.maharjan@palmmind.com>
2021-03-26 01:47:45 +0545

    adding components ans connecting theme

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <section>
-    <h2>My Friends</h2>
+    <header>
+      <h2>My Friends</h2>
+    </header>
+    <new-friend @add-contact="addContact"></new-friend>
     <ul>
       <friend-contact
         v-for="friend in friends"
@@ -40,6 +43,17 @@ export default {
       );
       identifiedFriend.isFavorite = !identifiedFriend.isFavorite;
     },
+    addContact(name, phone, email) {
+        const newFriendContact = {
+            id:new Date().toISOString(),
+            name:name,
+            phone:phone,
+            email:email,
+            isFavorite:false
+        }
+
+        this.friends.push(newFriendContact)
+    },
   },
   computed: {},
 };
@@ -71,7 +85,8 @@ header {
   padding: 0;
   list-style: none;
 }
-#app li {
+#app li,
+#app form {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
   border-radius: 10px;
@@ -100,5 +115,18 @@ header {
   background-color: #ec3169;
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+#app form div {
+  margin: 1rem 0;
 }
 </style>
