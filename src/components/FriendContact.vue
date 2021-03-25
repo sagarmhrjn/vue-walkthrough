5fd8676e31ff7e81d6e727598a5ee9a311f911c9
Sagar Maharjan <sagar.maharjan@palmmind.com>
2021-03-25 16:13:43 +0545

    adding a component

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong>{{ friend.phone }}</li>
+      <li><strong>Email:</strong>{{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+    
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "sagar",
+        name: "Sagar Maharjan",
+        phone: "9803690068",
+        email: "sagarmaharjan31@gmail.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
