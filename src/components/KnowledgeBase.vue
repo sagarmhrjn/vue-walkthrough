852443155ad78495fb9c206ec0a14d813412e209
Sagar Maharjan <sagar.maharjan@palmmind.com>
2021-03-26 16:45:28 +0545

    provide + inject to the rescue

--- src/components/KnowledgeBase.vue
+++ src/components/KnowledgeBase.vue
@@ -1,13 +1,15 @@
 <template>
   <section>
     <h2>Select a Topic</h2>
-    <knowledge-grid :topics="topics" @select-topic="$emit('select-topic', $event)"></knowledge-grid>
+    <knowledge-grid
+      @select-topic="$emit('select-topic', $event)"
+    ></knowledge-grid>
   </section>
 </template>
 
 <script>
 export default {
-  props: ['topics'],
-  emits: ['select-topic'],
+  // props: ["topics"],
+  emits: ["select-topic"],
 };
 </script>
