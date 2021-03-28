3242b1893de63489f991c9f43c9eaed5275e654a
Sagar Maharjan <sagar.maharjan@palmmind.com>
2021-03-28 14:59:09 +0545

    local vs globl components

--- src/main.js
+++ src/main.js
@@ -1,18 +1,11 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
-import ActiveElement from './components/ActiveElement.vue';
-import KnowledgeBase from './components/KnowledgeBase.vue';
-import KnowledgeElement from './components/KnowledgeElement.vue';
-import KnowledgeGrid from './components/KnowledgeGrid.vue';
+import BaseBadge from './components/BaseBadge.vue';
 
 const app = createApp(App);
 
-// All componentes are registered in this main.js file
-// so, they are available to this entore vue app
-app.component('active-element', ActiveElement);
-app.component('knowledge-base', KnowledgeBase);
-app.component('knowledge-element', KnowledgeElement);
-app.component('knowledge-grid', KnowledgeGrid);
+// registered as global component because it is being used in multiple parts of the app
+app.component('base-badge', BaseBadge);
 
 app.mount('#app');
