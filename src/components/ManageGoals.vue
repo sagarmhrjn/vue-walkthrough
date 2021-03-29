b947e46804286643090fb5c496670f492fa2c7a2
Sagar Maharjan <sagar.maharjan@palmmind.com>
2021-03-30 02:18:21 +0545

    applying what we know a probleml; erro-alert component

--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -1,4 +1,38 @@
 <template>
+  <div>
     <h2>Manage Goals</h2>
-    <input type="text">
+    <input type="text" ref="goal" />
+    <button @click="setGoal">Set Goal</button>
+    <error-alert v-if="inputIsInvalid">
+      <h2>Input is invalid!</h2>
+      <p>Please, enter a few characters</p>
+      <button @click="confirmError">Okay</button>
+    </error-alert>
+  </div>
 </template>
+
+<script>
+import ErrorAlert from "./ErrorAlert";
+
+export default {
+  components: {
+    ErrorAlert,
+  },
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
+  methods: {
+    setGoal() {
+      const enteredGoal = this.$refs.goal.value;
+      if (enteredGoal === "") {
+        this.inputIsInvalid = true;
+      }
+    },
+    confirmError(){
+        this.inputIsInvalid = false
+    }
+  },
+};
+</script>
