6b4a6a8fc9de33b4f840f5527d591fe2a87811b2
Sagar Maharjan <sagar.maharjan@palmmind.com>
2021-03-29 23:34:19 +0545

    introduction to slots

---
+++ src/components/BaseCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <!-- props are meant to be data which component need; 
+      slots are meant to be used for HTML code; a template code that component need-->
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["content"],
+};
+</script>
+
+<style scoped>
+div {
+  margin: 2rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+}
+</style>
