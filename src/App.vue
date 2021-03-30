49c0bff975c7142558db6e38af3884b076411a29
Sagar Maharjan <sagar.maharjan@palmmind.com>
2021-03-31 01:51:59 +0545

    project setup

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,26 @@
 <template>
-  <div>
-    <the-header></the-header>
-    <!-- <TheHeader /> -->
-    <!-- <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
-    <course-goals #default="slotProps">
-      <h2>{{ slotProps.item }}</h2>
-      <p>{{ slotProps['another-prop'] }}</p>
-    </course-goals> -->
-    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
-    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
-    <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
-    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals> -->
-    <!-- Dynamic components -->
-    <keep-alive>
-    <component :is="selectedComponent"></component>
-    </keep-alive>
-  </div>
+  <ul></ul>
 </template>
 
 <script>
-import TheHeader from "./components/layout/TheHeader";
-// import BadgeList from "./components/BadgeList";
-// import UserInfo from "./components/UserInfo";
-// import CourseGoals from "./components/CourseGoals"
-import ManageGoals from "./components/ManageGoals";
-import ActiveGoals from "./components/ActiveGoals";
-
 export default {
-  // local components
-  components: {
-    // "the-header": TheHeader,
-    TheHeader,
-    // BadgeList,
-    // UserInfo,
-    // CourseGoals,
-    ManageGoals,
-    ActiveGoals,
-  },
-  data() {
-    return {
-      selectedComponent: "active-goals",
-      activeUser: {
-        name: "Sagar Maharjan",
-        description: "Site owner and admin",
-        role: "admin",
-      },
-    };
-  },
-  methods: {
-    setSelectedComponent(cmp) {
-      this.selectedComponent = cmp;
-    },
-  },
-};
+  data(){
+    return{
+      storedResources:[
+        {
+          id:'official-guide',
+          title:'Official Guide',
+          description:'The official Vue.s documentation',
+          link:'https://vuejs.org'
+        },
+        {
+          id:'google',
+          title:'Google',
+          description:'Learn to Google',
+          link:'https://google.com'
+        }
+      ]
+    }
+  }
+}
 </script>
-
-<style>
-html {
-  font-family: sans-serif;
-}
-
-body {
-  margin: 0;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,7 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
-import BaseBadge from './components/UI/BaseBadge.vue';
-import BaseCard from './components/UI/BaseCard.vue';
 
 const app = createApp(App);
-
-// registered as global component because it is being used in multiple parts of the app
-app.component('base-badge', BaseBadge);
-app.component('base-card', BaseCard);
 
 app.mount('#app');
