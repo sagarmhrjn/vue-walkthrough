2474d83d716286539c5fedce9946ee4cdb5e4257
Sagar Maharjan <sagar.maharjan@palmmind.com>
2021-04-01 02:50:40 +0545

    adding a modal dialog

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import { createApp } from 'vue';
 import App from './App';
 import BaseCard from './components/UI/BaseCard'
 import BaseButton from './components/UI/BaseButton'
+import BaseDialog from './components/UI/BaseDialog'
 
 const app = createApp(App);
 
 // global components;base card might be used in other part of the app as well; 
 app.component('base-card', BaseCard)
 app.component('base-button', BaseButton)
+app.component('base-dialog', BaseDialog)
 
 app.mount('#app');
