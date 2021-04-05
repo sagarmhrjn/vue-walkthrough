aa4fefa8bec7dddd13b89b962ba06fb592c8760d
Sagar Maharjan <sagar.maharjan@palmmind.com>
2021-04-06 00:10:53 +0545

    v-model and inputs

--- src/main.js
+++ src/main.js
@@ -1,15 +1,5 @@
 import { createApp } from 'vue';
 
-import App from './App';
-import BaseCard from './components/UI/BaseCard'
-import BaseButton from './components/UI/BaseButton'
-import BaseDialog from './components/UI/BaseDialog'
+import App from './App.vue';
 
-const app = createApp(App);
-
-// global components;base card might be used in other part of the app as well; 
-app.component('base-card', BaseCard)
-app.component('base-button', BaseButton)
-app.component('base-dialog', BaseDialog)
-
-app.mount('#app');
+createApp(App).mount('#app');
