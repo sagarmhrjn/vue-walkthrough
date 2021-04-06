1a3450f1d43499915542802f01d73da305ea5575
Sagar Maharjan <sagar.maharjan@palmmind.com>
2021-04-06 11:33:27 +0545

    routing setup and install vue-router dependency pacakge for routing

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
 
+const router = createRouter({
+    // by calling createWebHistory , it tells that just use the browser built-in mechanism
+    history: createWebHistory(),
+    // routes array is the heart of the router; which component should be loaded on which URL
+    routes: []
+})
 const app = createApp(App)
 
 app.mount('#app');
