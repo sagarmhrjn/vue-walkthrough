905301cfce9c200b09c322172aefbfa4b17be082
Sagar Maharjan <sagar.maharjan@palmmind.com>
2021-04-06 12:43:58 +0545

    passing data with route params (dynamic segments)

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
 import TeamsListComponent from './components/teams/TeamsList.vue'
 import UsersListComponent from './components/users/UsersList.vue'
+import TeamMembersComponent from './components/teams/TeamMembers.vue'
 
 const router = createRouter({
     // by calling createWebHistory , it tells that just use the browser built-in mechanism
@@ -18,6 +19,10 @@ const router = createRouter({
         {
             path: '/users',
             component: UsersListComponent
+        },
+        {
+            path: '/teams/:teamId',
+            component: TeamMembersComponent
         }
     ],
     // if we wanna use router link exact active class
