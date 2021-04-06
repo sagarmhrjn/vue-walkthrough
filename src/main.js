46f48350ea278862020bce93ede98071458126f8
Sagar Maharjan <sagar.maharjan@palmmind.com>
2021-04-06 13:44:06 +0545

    redirecting and catch all routes

--- src/main.js
+++ src/main.js
@@ -5,12 +5,18 @@ import App from './App.vue';
 import TeamsListComponent from './components/teams/TeamsList.vue'
 import UsersListComponent from './components/users/UsersList.vue'
 import TeamMembersComponent from './components/teams/TeamMembers.vue'
+import NotFound from './components/nav/NotFound'
 
 const router = createRouter({
     // by calling createWebHistory , it tells that just use the browser built-in mechanism
     history: createWebHistory(),
     // routes array is the heart of the router; which component should be loaded on which URL
     routes: [
+        // you can use alias too
+        {
+            path: '/',
+            redirect: '/teams'
+        },
         // ourdomain.com/teams =>TeamsList
         {
             path: '/teams',
@@ -24,6 +30,10 @@ const router = createRouter({
             path: '/teams/:teamId',
             component: TeamMembersComponent,
             props: true
+        },
+        {
+            path: '/:notFound(.*)',
+            component: NotFound
         }
     ],
     // if we wanna use router link exact active class
