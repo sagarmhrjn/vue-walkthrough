95db2a73bc095eaf807e2cbb5c703e9a57280fda
Sagar Maharjan <sagar.maharjan@palmmind.com>
2021-04-06 11:59:28 +0545

    registering and redering routes

--- src/main.js
+++ src/main.js
@@ -2,13 +2,27 @@ import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
+import TeamsListComponent from './components/teams/TeamsList.vue'
+import UsersListComponent from './components/users/UsersList.vue'
 
 const router = createRouter({
     // by calling createWebHistory , it tells that just use the browser built-in mechanism
-    history: createWebHistory(),
+    history: createWebHistory('/vue'),
     // routes array is the heart of the router; which component should be loaded on which URL
-    routes: []
+    routes: [
+        // ourdomain.com/teams =>TeamsList
+        {
+            path: '/teams',
+            component: TeamsListComponent
+        },
+        {
+            path: '/users',
+            component: UsersListComponent
+        }
+    ]
 })
 const app = createApp(App)
+
+app.use(router)
 
 app.mount('#app');
