fe9fcb597e4e7f9e457a109c84162ece724efc93
Sagar Maharjan <sagar.maharjan@palmmind.com>
2021-04-06 12:10:59 +0545

    navigation with router link

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import UsersListComponent from './components/users/UsersList.vue'
 
 const router = createRouter({
     // by calling createWebHistory , it tells that just use the browser built-in mechanism
-    history: createWebHistory('/vue'),
+    history: createWebHistory(),
     // routes array is the heart of the router; which component should be loaded on which URL
     routes: [
         // ourdomain.com/teams =>TeamsList
