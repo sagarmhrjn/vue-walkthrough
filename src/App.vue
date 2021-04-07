1f5c449f0b66dc119dced62be416f3fa8d7959a5
Sagar Maharjan <sagar.maharjan@palmmind.com>
2021-04-07 16:25:20 +0545

    getting started with animated lists

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="container">
+    <users-list></users-list>
+  </div>
+  <div class="container">
     <div class="block" :class="{ animate: animatedBlock }"></div>
     <button @click="animateBlock">Animate</button>
   </div>
@@ -43,7 +46,12 @@
 </template>  
 
 <script>
+import UsersList from "./components/UsersList";
+
 export default {
+  components:{
+    UsersList
+  },
   data() {
     return {
       animatedBlock: false,
---
+++ src/components/UsersList.vue
@@ -0,0 +1,44 @@
+<template>
+  <ul>
+    <li v-for="user in users" :key="user" @click="removeUser(user)">
+      {{ user }}
+    </li>
+  </ul>
+  <div>
+    <input type="text" ref="userNameInput" />
+    <button @click="addUser">Add User</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      users: ["Sagar", "Bikash", "Ashish"],
+    };
+  },
+  methods: {
+    addUser() {
+      const enteredUserName = this.$refs.userNameInput.value;
+      this.users.unshift(enteredUserName);
+    },
+    removeUser(user) {
+      this.users = this.users.filter((usr) => user != usr);
+    },
+  },
+};
+</script>
+
+<style >
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+li {
+  border: 1px solid #ccc;
+  padding: 1rem;
+  text-align: center;
+}
+</style>
