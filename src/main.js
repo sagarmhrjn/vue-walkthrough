264508b3db657f324b374a7f651d395443b501d3
Sagar Maharjan <sagar.maharjan@palmmind.com>
2021-04-08 03:05:49 +0545

    getters - a better way of getting data

--- src/main.js
+++ src/main.js
@@ -11,12 +11,30 @@ const store = createStore({
         }
     },
     // clearly defined methods which have the logic to update the state
+    // same as reducers
     mutations: {
         increment(state) {
             state.counter = state.counter + 2
         },
         increase(state, payload) {
             state.counter = state.counter + payload.value
+        }
+    },
+    // think of them as computed properties for stores.
+    // same as selectors
+    getters: {
+        finalCounter(state) {
+            return state.counter * 2
+        },
+        normalizedCounter(_, getters) {
+            const finalCounter = getters.finalCounter
+            if (finalCounter < 0) {
+                return 0
+            }
+            if (finalCounter > 100) {
+                return 100
+            }
+            return finalCounter
         }
     }
 })
