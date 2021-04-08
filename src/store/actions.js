24a05628fb538581ad89781481fb25a68bb918d7
Sagar Maharjan <sagar.maharjan@palmmind.com>
2021-04-08 15:12:31 +0545

    challenge shopping cart item

--- src/store/actions.js
+++
@@ -1,8 +0,0 @@
-export default {
-    login(context) {
-        context.commit('setAuth', { auth: true })
-    },
-    logout(context) {
-        context.commit('setAuth', { auth: false })
-    }
-}
--- src/store/modules/counter/mutations.js
+++
@@ -1,8 +0,0 @@
-export default {
-    increment(state) {
-        state.counter = state.counter + 2
-    },
-    increase(state, payload) {
-        state.counter = state.counter + payload.value
-    },
-}
