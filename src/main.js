f8ad64dd7af93509446f510b160fe346a1e86034
Sagar Maharjan <sagar.maharjan@palmmind.com>
2021-04-08 15:41:39 +0545

    installed vuex package; created module store for cart and products

--- src/main.js
+++ src/main.js
@@ -4,9 +4,13 @@ import router from './router.js';
 import App from './App.vue';
 import BaseBadge from './components/ui/BaseBadge.vue';
 
+import store from './store'
+
 const app = createApp(App)
 
 app.use(router);
+
+app.use(store)
 
 app.component('base-badge', BaseBadge);
 
